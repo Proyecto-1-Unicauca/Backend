dd4e34b11dd6a8d13e6ca02f1f285f07b8b31cbe
Nisos <josess.nicolas@hotmail.com>
2022-02-09 14:20:25 -0500

    Get subject and create courses

--- lab_manager/urls.py
+++ lab_manager/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("workshops", views.workshops, name="workshops"),
     path("validate-email", views.validate_email, name="validate-email"),
+    path("courses", views.courses, name="courses"),
     path("subjects", views.subjects, name="subjects")
 ]
--- lab_manager/views.py
+++ lab_manager/views.py
@@ -11,8 +11,10 @@ cred = credentials.Certificate('PrivateKey.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+
 def index(request):
     return render(request, "general/index.html")
+
 
 @csrf_exempt
 def workshops(request):
@@ -29,11 +31,12 @@ def workshops(request):
 
         try:
             db.collection(u'workshop').add(newWorkshop)
-            return JsonResponse({ "message": "User created" }, status=201)
+            return JsonResponse({"message": "User created"}, status=201)
         except ValueError:
-            return JsonResponse({ "message": "User not created" }, status=201)
+            return JsonResponse({"message": "User not created"}, status=201)
     else:
         return JsonResponse({"message": "Invalid action"}, status=201)
+
 
 @csrf_exempt
 def validate_email(request):
@@ -44,26 +47,31 @@ def validate_email(request):
         docs = teachers_ref.stream()
         query_ref = teachers_ref.where(u'email', u'==', email).limit(1).get()
 
-        if(query_ref):
+        if (query_ref):
             for teacher in query_ref:
-                return JsonResponse({"message": "User found", "userId": teacher.id, "username": teacher.to_dict()['name']}, status=201)
+                return JsonResponse(
+                    {"message": "User found", "userId": teacher.id, "username": teacher.to_dict()['name']}, status=201)
         else:
             return JsonResponse({"message": "User not found"}, status=201)
     else:
         return JsonResponse({"message": "Invalid action"}, status=201)
 
+
 @csrf_exempt
-def subjects(request):
+def courses(request):
     if request.method == "POST":
         data = json.loads(request.body.decode("utf-8"))
 
-        newSubject = {
-            u'id': data['id'],
-            u'name': data['name']
+        newCourse = {
+            u'subject_id': data['subject_id'],
+            u'name': data['name'],
+            u'start': data['start'],
+            u'end': data['end']
         }
+
         try:
-            db.collection(u'subjects').add(newSubject)
-            return JsonResponse({"message": "new subject registered"}, status=201)
+            db.collection(u'courses').add(newCourse)
+            return JsonResponse({"message": "new course registered"}, status=201)
         except ValueError:
             return JsonResponse({"message": "error creating new subject"}, status=201)
     elif request.method == "GET":
@@ -74,9 +82,31 @@ def subjects(request):
 
         if query_ref:
             for subject in query_ref:
-                return JsonResponse({"message": "Subject found", "subjectId": subject.id, "subjectName": subject.to_dict()['name']}, status=201)
+                return JsonResponse(
+                    {"message": "Subject found", "subjectId": subject.id, "subjectName": subject.to_dict()['name']},
+                    status=201)
         else:
             return JsonResponse({"message": "Subject not found"}, status=201)
     else:
         return JsonResponse({"message": "Invalid action"}, status=201)
 
+
+def subjects(request):
+    if request.method == "GET":
+        subject_ref = db.collection(u'subjects')
+        query_ref = subject_ref.get()
+
+        subject_dict = {}
+        subject_records = []
+
+        if query_ref:
+            for subject in query_ref:
+                subject = {"subjectId": subject.id, "subjectName": subject.to_dict()['name']}
+                subject_records.append(subject)
+
+            subject_dict["subject"] = subject_records
+            return JsonResponse(subject_dict)
+        else:
+            return JsonResponse({"message": "Subject not found"}, status=201)
+    else:
+        return JsonResponse({"message": "Invalid action"}, status=201)
