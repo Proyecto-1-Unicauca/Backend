b36d0ccb3e69d77411392b2fa65c9776ca85e048
Nisos <josess.nicolas@hotmail.com>
2022-02-09 21:33:34 -0500

    Update views.py

--- lab_manager/views.py
+++ lab_manager/views.py
@@ -1,4 +1,6 @@
+import google.cloud.firestore_v1.field_path
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
@@ -6,6 +8,7 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud.firestore_v1.field_path import FieldPath
 
 cred = credentials.Certificate('PrivateKey.json')
 firebase_admin.initialize_app(cred)
@@ -62,11 +65,14 @@ def courses(request):
     if request.method == "POST":
         data = json.loads(request.body.decode("utf-8"))
 
+        access_key = get_random_string(length=8)
         newCourse = {
+            u'access_key': access_key,
             u'subject_id': data['subject_id'],
             u'name': data['name'],
             u'start': data['start'],
-            u'end': data['end']
+            u'end': data['end'],
+            u'teacher_id': 104615020640
         }
 
         try:
@@ -74,21 +80,45 @@ def courses(request):
             return JsonResponse({"message": "new course registered"}, status=201)
         except ValueError:
             return JsonResponse({"message": "error creating new subject"}, status=201)
-    elif request.method == "GET":
+
+    if request.method == "GET":
         data = json.loads(request.body.decode("utf-8"))
-        id_get = data['id']
-        subject_ref = db.collection(u'subjects')
-        query_ref = subject_ref.where(u'id', u'==', id_get).limit(1).get()
+        teacher_id = data['teacher_id']
+        courses_ref = db.collection(u'courses')
+        query_ref = courses_ref.where(u'teacher_id', u'==', teacher_id).get()
+
+        teacher_courses_dict = {}
+        teacher_courses_records = []
 
         if query_ref:
-            for subject in query_ref:
-                return JsonResponse(
-                    {"message": "Subject found", "subjectId": subject.id, "subjectName": subject.to_dict()['name']},
-                    status=201)
+            for course in query_ref:
+                teacher_course = {"courseId": course.id, "accessKey": course.to_dict()['access_key'],
+                                  "name": course.to_dict()['name'], "start": course.to_dict()['start'],
+                                  "end": course.to_dict()['end'], "subjectId": course.to_dict()['subject_id']}
+                teacher_courses_records.append(teacher_course)
+
+            teacher_courses_dict["courses"] = teacher_courses_records
+            return JsonResponse(teacher_courses_dict, status=201)
         else:
-            return JsonResponse({"message": "Subject not found"}, status=201)
-    else:
-        return JsonResponse({"message": "Invalid action"}, status=201)
+            return JsonResponse({"message": "Teacher course not found"}, status=201)
+
+    if request.method == "DELETE":
+        data = json.loads(request.body.decode("utf-8"))
+        course_id = data['course_id']
+        courses_ref = db.collection(u'courses')
+        query_ref = courses_ref.where(FieldPath.document_id(), u'==', course_id).limit(1).get()
+
+        if query_ref:
+            for course in query_ref:
+                try:
+                    course.delete()
+                    return JsonResponse({"message": "Course deleted"}, status=201)
+                except ValueError:
+                    return JsonResponse({"message": "error deleting course"}, status=201)
+        else:
+            return JsonResponse({"message": "course not found"}, status=201)
+
+    return JsonResponse({"message": "Invalid action"}, status=201)
 
 
 def subjects(request):
@@ -104,8 +134,8 @@ def subjects(request):
                 subject = {"subjectId": subject.id, "subjectName": subject.to_dict()['name']}
                 subject_records.append(subject)
 
-            subject_dict["subject"] = subject_records
-            return JsonResponse(subject_dict)
+            subject_dict["subjects"] = subject_records
+            return JsonResponse(subject_dict, status=201)
         else:
             return JsonResponse({"message": "Subject not found"}, status=201)
     else:
